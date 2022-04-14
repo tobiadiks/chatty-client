6321da62d35a3aca96e6cff9ec5d94d1292350aa
tobiadiks <tobiadiks@gmail.com>
2022-04-14 18:11:46 +0100

    up

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -4,7 +4,7 @@ const ContactPage=()=>{
     return(
         <MainLayout>
             <div>
-                <div className="w-8 h-8"></div>
+                <div className="w-8 h-8 rounded-full bg-ye"></div>
             </div>
         </MainLayout>
     )
