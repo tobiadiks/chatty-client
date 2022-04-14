ca2d694a21eb77f7ec2ce8dd8cbc57f774a55768
tobiadiks <tobiadiks@gmail.com>
2022-04-14 18:11:11 +0100

    added contact

---
+++ pages/contacts.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "../components/layout/main"
+
+const ContactPage=()=>{
+    return(
+        <MainLayout>
+            <div>
+                <div className="w-8 h-8"></div>
+            </div>
+        </MainLayout>
+    )
+}
